3174241272465e4bc2cc3d0a9dbb898d28e65e2d
belladonnagiacomo <belladonna.giacomo@franchettisalviani.net>
2026-02-23 08:58:23 +0100

    mod

--- esA/Program.cs
+++ esA/Program.cs
@@ -9,19 +9,36 @@
             {
                 vettoreA[i] = rnd.Next(100, 1000);
                 vettoreB[i] = 0;
-                Console.WriteLine(vettoreA[i]);
-                Console.WriteLine(vettoreB[i]);
-                
-            
+
+                Console.WriteLine("vettoreA " + vettoreA[i]);
+             
             }
 
-
+            
+        }
+         
+        static void cumulativa(int[] vettoreA, int[] vettoreB)
+        {
+            popolaVendite(vettoreA, vettoreB);
+            Console.WriteLine("---------------------");
+            for (int i = 0; i < vettoreB.Length; i++)
+            {
+                if (i == 0)
+                {
+                    vettoreB[i] = vettoreA[i];
+                }
+                if (i > 0)
+                {
+                    vettoreB[i] = vettoreA[i] + vettoreB[i - 1];
+                }
+                Console.WriteLine("vettoreB " + vettoreB[i]);
+            }
         }
         static void Main(string[] args)
         {
             int[] vettoreA = new int[12];
             int[] vettoreB = new int[12];
-            popolaVendite(vettoreA, vettoreB);
+            cumulativa(vettoreA, vettoreB);
         }
     }
 }
