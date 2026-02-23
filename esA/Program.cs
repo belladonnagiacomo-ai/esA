d2548df2bd02f8091e756346aeb5bd13d0ed29ab
belladonnagiacomo <belladonna.giacomo@franchettisalviani.net>
2026-02-23 08:40:04 +0100

    mod

--- esA/Program.cs
+++ esA/Program.cs
@@ -2,9 +2,26 @@
 {
     internal class Program
     {
+        static void popolaVendite(int[] vettoreA,  int[] vettoreB){
+
+            Random rnd = new Random();
+            for (int i = 0; i < vettoreA.Length; i++)
+            {
+                vettoreA[i] = rnd.Next(100, 1000);
+                vettoreB[i] = 0;
+                Console.WriteLine(vettoreA[i]);
+                Console.WriteLine(vettoreB[i]);
+                
+            
+            }
+
+
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] vettoreA = new int[12];
+            int[] vettoreB = new int[12];
+            popolaVendite(vettoreA, vettoreB);
         }
     }
 }
